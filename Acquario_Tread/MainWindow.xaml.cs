3930b95670abc9636c40b276fc38a4b7611973af
Lor3nzoF4bbr1 <lorenzo.fabbri01.stud@ispascalcomandini.it>
2020-12-16 11:33:45 +0100

    Step 1: Lezione sull'introduzione al movimento

---
+++ Acquario_Tread/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Threading;
+
+namespace Mare_Thread
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Uri uriPesce1 = new Uri("Phish.png", UriKind.Relative);
+        public int posPesce1 = 0;
+        public MainWindow()
+        {
+            InitializeComponent(); 
+
+            Thread t1 = new Thread(new ThreadStart(muoviPesce1));
+
+            ImageSource imm = new BitmapImage(uriPesce1);
+            imgPesce1.Source = imm;
+
+            t1.Start();
+        }
+
+        public void muoviPesce1()
+        {
+            while(posPesce1<500)
+            {
+                posPesce1 += 100;
+
+                Thread.Sleep(TimeSpan.FromMilliseconds(700));
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    imgPesce1.Margin = new Thickness(posPesce1, 300, 0, 0);
+                }));
+            }
+        }
+    }
+}
